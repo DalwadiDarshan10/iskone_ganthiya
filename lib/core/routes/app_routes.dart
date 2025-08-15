e3a4c78a987451ae35e7d3b256fa499e0e819cbe
Darshan Dalwadi <dalwadidarshan1010@gmail.com>
2025-08-15 10:18:33 +0530

    start projrct from folder structure

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,10 @@
+
+import 'package:iskon_ganthiya/core/routes/routes_name.dart';
+
+class AppRoutes {
+  static const initial = PATH.spalshScreen;
+  static final routes = {
+    
+  }
+
+}
---
+++ lib/core/routes/routes_name.dart
@@ -0,0 +1,17 @@
+
+
+abstract class PATH{
+  static const spalshScreen = _PATH.spalshScreen;
+   static const loginScreen = _PATH.login;
+    static const signUpScreen = _PATH.signup;
+     static const homeScreen = _PATH.home;
+
+}
+
+abstract class _PATH{
+  static const spalshScreen = "/spashScreen";
+ static const login  = "/login";
+ static const signup = "/signup";
+  static const home = "/home";
+  
+}
